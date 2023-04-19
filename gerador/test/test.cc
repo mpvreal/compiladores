b27b953fb050638dc4b46d8eca2a4774dbc59ea4
Matheus Pires Vila Real <matheus.pires.vila@uel.br>
2023-04-19 08:54:29 -0300

    Antes de trocar acesso a memória

--- gerador/test/test.cc
+++ gerador/test/test.cc
@@ -1,12 +1,29 @@
 #include <iostream>
-#include <list>
+#include <vector>
+
+int offset(const std::vector<int>& dimensions, const std::vector<int>& indexes) {
+    int sum = 0,
+        product = 1;
+
+    for(int k = 0; k < dimensions.size(); k++) {
+        for(int l = k + 1; l < dimensions.size(); l++) {
+            product *= dimensions[l];
+        }
+        sum += indexes[k] * product;
+        product = 1;
+    }
+
+    return sum;
+}
 
 int main() {
-    std::list<const void*> l;
+    int teste[3][3][3] = { { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }, 
+                            { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} }, 
+                            { {19, 20, 21}, {22, 23, 24}, {25, 26, 27} } };
+    int flat[27] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 
+        23, 24, 25, 26, 27 };
 
-    for(const void*& i : l) {
-        *((int*) i);
-    }
+    std::cout << flat[offset({3, 3, 3}, {1, 2, 1})] << " = " << teste[1][2][1] << std::endl;
 
     return 0;
 }
